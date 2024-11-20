aa9fbab3b9abfd47effc13c2cd8746af0524989a
AbdujalolAndy <mrarnoldunlimited@gmail.com>
2024-11-20 19:19:45 +0900

    feat: Develop sectional events components

--- src/app/screens/HomePage/ActiveUsers.tsx
+++ src/app/screens/HomePage/ActiveUsers.tsx
@@ -1,10 +1,51 @@
-import React from "react";
-import { Container } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
+import Card from "@mui/joy/Card";
+import { CssVarsProvider, Typography } from "@mui/joy";
+import CardOverflow from "@mui/joy/CardOverflow";
+import AspectRatio from "@mui/joy/AspectRatio";
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+
 
 export default function ActiveUsers() {
+    const topUsers: any[] = []
+
     return (
-        <Container>
-            ActiveUsers
-        </Container>
-    )
+        <div className={"active-users-frame"}>
+            <Container>
+                <Stack className={"main"}>
+                    <Box className={"category-title"}>Active Users</Box>
+                    <Stack className={"cards-frame"}>
+                        <CssVarsProvider>
+                            {topUsers.length !== 0 ? (
+                                topUsers.map((member: any) => {
+                                    const imagePath = "/img/default-user.svg";
+                                    return (
+                                        <Card
+                                            key={member._id}
+                                            variant="outlined"
+                                            className={"card"}
+                                        >
+                                            <CardOverflow>
+                                                <AspectRatio ratio="1">
+                                                    <img src={imagePath} alt="" />
+                                                </AspectRatio>
+                                            </CardOverflow>
+                                            <CardOverflow>
+                                                <Typography className={"member-nickname"}>
+                                                    {member.memberNick}
+                                                </Typography>
+                                            </CardOverflow>
+                                        </Card>
+                                    );
+                                })
+                            ) : (
+                                <Box className="no-data">No Active Users!</Box>
+                            )}
+                        </CssVarsProvider>
+                    </Stack>
+                </Stack>
+            </Container>
+        </div>
+    );
 }
