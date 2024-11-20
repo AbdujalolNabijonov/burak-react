f1d261a0050620c74237c9c461a91e793a012e73
AbdujalolAndy <mrarnoldunlimited@gmail.com>
2024-11-20 18:28:48 +0900

    feat: HomePage related components

---
+++ src/app/screens/HomePage/NewDishes.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "@mui/material";
+
+export default function NewDishes() {
+    return (
+        <Container>
+            NewDishes
+        </Container>
+    )
+}
--- src/app/screens/HomePage/index.tsx
+++ src/app/screens/HomePage/index.tsx
@@ -1,10 +1,21 @@
 import { Container } from "@mui/material"
+import Statistics from "./Statistics"
+import PopularDishes from "./PopularDishes"
+import NewDishes from "./NewDishes"
+import Advertisement from "./Advertisement"
+import ActiveUsers from "./ActiveUsers"
+import Events from "./Events"
 
 const HomePage = (props: any) => {
     return (
-        <Container>
-            <h1>HomePage</h1>
-        </Container>
+        <div className="homepage">
+            <Statistics />
+            <PopularDishes />
+            <NewDishes />
+            <Advertisement />
+            <ActiveUsers />
+            <Events />
+        </div>
     )
 }
 
