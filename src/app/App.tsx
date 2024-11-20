7504dd98c7d06bab4ef97ca80d2198e4d7ce7422
AbdujalolAndy <mrarnoldunlimited@gmail.com>
2024-11-20 18:03:25 +0900

    feat: Publishing HomenNavbar & Basket

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Button, Container, Stack } from '@mui/material';
 import { Link, Route, Switch, useLocation } from 'react-router-dom';
-import '../css/app.css';
 import ProductsPage from './screens/productsPage';
 import HelpPage from './screens/helpPage';
 import HomePage from './screens/homePage';
-import { HomeNavbar } from './components/headers/HomeNavbar';
 import { OtherNavbar } from './components/headers/OtherNavbar';
-import { Footer } from './components/footer';
+import Footer from './components/footer';
+
+import '../css/app.css';
+import "../css/navbar.css";
+import "../css/footer.css";
+import HomeNavbar from './components/headers/HomeNavbar';
 
 function App() {
   //Initializations
